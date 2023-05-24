3a98fdfddb31775bc53909905b5f186a9837bdf2
karthiksk6006 <karthiksk627857@gmail.com>
2023-05-24 10:33:00 +0530

    update one

--- src/test/java/org/karthk/Task.java
+++ src/test/java/org/karthk/Task.java
@@ -1,5 +1,32 @@
 package org.karthk;
 
-public class Task {
+import org.baseclass.BaseClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+public class Task extends BaseClass {
+
+	@BeforeClass
+	public static  void browserlaumch() {
+      browserlaunch("edge");
+      maximize();
+      implicityWait(10);
+      urlLaunch("https://www.facebook.com/");
+      
+	}
+@Test
+public  void login() {
+   WebElement mail = driver.findElement(By.id("email"));
+   sendkeys(mail, "karthik");
+   WebElement pass = driver.findElement(By.id("pass"));
+   sendkeys(pass, "1111111");
+   WebElement login = driver.findElement(By.name("login"));
+   click(login);
+}
+	
+	
+	
+	
 }
